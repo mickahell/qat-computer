4cbae624ec46f51b91f311271675a08a8874770d
Michaël Rollin <mika89700@gmail.com>
2023-10-03 23:22:04 +0200

    Init go binary + github actions (#8)
    ### Summary
    
    Init the go binary by setting basic app init & add github actions for go tests and lint.
    
    ### Details
    
    - [x] Init go binary
    - [x] Add github actions for go
    
    ---
    Closes #4

---
+++ src/docs/docs.go
@@ -0,0 +1,53 @@
+package docs
+
+import (
+	_ "embed"
+
+	"cat-computer/logger"
+)
+
+//go:embed VERSION.txt
+var version_file string
+var app_name string = "Qat Computer"
+
+const draw = `
+  #####                   #####
+ #     #   ##   #####    #     #  ####  #    # #####  #    # ##### ###### #####
+ #     #  #  #    #      #       #    # ##  ## #    # #    #   #   #      #    #
+ #     # #    #   #      #       #    # # ## # #    # #    #   #   #####  #    #
+ #   # # ######   #      #       #    # #    # #####  #    #   #   #      #####
+ #    #  #    #   #      #     # #    # #    # #      #    #   #   #      #   #
+  #### # #    #   #       #####   ####  #    # #       ####    #   ###### #    #
+
+by Mickahell.
+`
+
+func DrawStart() {
+	logger.GetLogger().LogDraw(draw)
+	logger.GetLogger().LogDraw("Version : " + version_file + "\n")
+}
+
+func GetVersion() string {
+	return version_file
+}
+
+func GetAppName() string {
+	return app_name
+}
+
+func GetConfigPathMan() string {
+	man := "Relative path for the configuration yaml file.\n" +
+		"The configuration file can contains any options presented below."
+
+	return man
+}
+
+func GetLogLevelMan() string {
+	man := "level of log to print :\n" +
+		"0 : Informational\n" +
+		"1 : Warning\n" +
+		"2 : Error --> Always shown\n" +
+		"3 : Critical --> Always shown"
+
+	return man
+}
