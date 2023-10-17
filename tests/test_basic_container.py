50e0fc6927db2d41110faca79abab7491a509db0
Mica <mika89700@gmail.com>
2023-10-17 21:32:26 +0200

    lint

--- tests/test_basic_container.py
+++ tests/test_basic_container.py
@@ -1,13 +1,18 @@
 """Tests for basic container."""
 import os
-import docker
 from unittest import TestCase
+import docker
 
 
 def run_container(
     docker_env: docker.DockerClient, current_directory: str
 ) -> docker.DockerClient.containers:
     """Create container function."""
+    try:
+        docker_env.images.get("qatcomputer")
+    except docker.errors.ImageNotFound:
+        build_image(docker_env=docker_env, current_directory=current_directory)
+
     return docker_env.containers.run(
         image="qatcomputer",
         name="qatcompter",
@@ -25,7 +30,7 @@ def run_container(
 
 def build_image(docker_env: docker.DockerClient, current_directory: str):
     """Build the image function."""
-    img, _ = docker_env.images.build(
+    docker_env.images.build(
         path=f"{current_directory}/../",
         dockerfile=f"{current_directory}/../docker/Dockerfile",
         tag="qatcomputer",
@@ -39,24 +44,18 @@ class TestBasicContainer(TestCase):
         """SetUp container object."""
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.build_img = False
-        self.BINARY = "qat-computer"
-        self.NOFLAGCMD = ""
+        self.binary = "qat-computer"
+        self.noflagcmd = ""
         self.client = docker.from_env()
-        try:
-            self.client.images.get("qatcomputer")
-        except:
-            build_image(
-                docker_env=self.client, current_directory=self.current_directory
-            )
         self.container = run_container(
             docker_env=self.client, current_directory=self.current_directory
         )
 
     def test_version_endpoint(self):
         """Test version endpoint."""
-        self.NOFLAGCMD = "-version"
+        self.noflagcmd = "-version"
         exit_code, output = self.container.exec_run(
-            cmd=f"{self.BINARY} {self.NOFLAGCMD}", tty=True
+            cmd=f"{self.binary} {self.noflagcmd}", tty=True
         )
 
         self.assertEqual(exit_code, 0)
@@ -69,9 +68,9 @@ class TestBasicContainer(TestCase):
 
     def test_qiskit_version_endpoint(self):
         """Test qiskit version endpoint."""
-        self.NOFLAGCMD = "-qiskit-version"
+        self.noflagcmd = "-qiskit-version"
         exit_code, output = self.container.exec_run(
-            cmd=f"{self.BINARY} {self.NOFLAGCMD}", tty=True
+            cmd=f"{self.binary} {self.noflagcmd}", tty=True
         )
 
         self.assertEqual(exit_code, 0)
@@ -79,9 +78,9 @@ class TestBasicContainer(TestCase):
 
     def test_os_version_endpoint(self):
         """Test os version endpoint."""
-        self.NOFLAGCMD = "-os-version"
+        self.noflagcmd = "-os-version"
         exit_code, output = self.container.exec_run(
-            cmd=f"{self.BINARY} {self.NOFLAGCMD}", tty=True
+            cmd=f"{self.binary} {self.noflagcmd}", tty=True
         )
 
         self.assertEqual(exit_code, 0)
@@ -89,9 +88,9 @@ class TestBasicContainer(TestCase):
 
     def test_conf_endpoint(self):
         """Test conf endpoint."""
-        self.NOFLAGCMD = "-show-config -conf=/etc/qat-computer/conf/conf_docker.yaml"
+        self.noflagcmd = "-show-config -conf=/etc/qat-computer/conf/conf_docker.yaml"
         exit_code, output = self.container.exec_run(
-            cmd=f"{self.BINARY} {self.NOFLAGCMD}", tty=True
+            cmd=f"{self.binary} {self.noflagcmd}", tty=True
         )
 
         self.assertEqual(exit_code, 0)
--- tests/test_compute_container.py
+++ tests/test_compute_container.py
@@ -1,9 +1,9 @@
 """Tests for compute subcmd container."""
 import os
-import docker
 from unittest import TestCase
+import docker
 
-from .test_basic_container import run_container, build_image
+from .test_basic_container import run_container
 
 
 class TestComputeContainer(TestCase):
@@ -13,25 +13,19 @@ class TestComputeContainer(TestCase):
         """SetUp compute container object."""
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.build_img = False
-        self.BINARY = "qat-computer"
-        self.SUBCMD = "compute"
-        self.FLAGCMD = ""
+        self.binary = "qat-computer"
+        self.subcmd = "compute"
+        self.flagcmd = ""
         self.client = docker.from_env()
-        try:
-            self.client.images.get("qatcomputer")
-        except:
-            build_image(
-                docker_env=self.client, current_directory=self.current_directory
-            )
         self.container = run_container(
             docker_env=self.client, current_directory=self.current_directory
         )
 
     def test_full_endpoint(self):
         """Test full compute endpoint."""
-        self.FLAGCMD = "-conf /etc/qat-computer/conf/conf_docker.yaml"
+        self.flagcmd = "-conf /etc/qat-computer/conf/conf_docker.yaml"
         exit_code, output = self.container.exec_run(
-            cmd=f"{self.BINARY} {self.SUBCMD} {self.FLAGCMD}", tty=True
+            cmd=f"{self.binary} {self.subcmd} {self.flagcmd}", tty=True
         )
 
         self.assertEqual(exit_code, 0)
