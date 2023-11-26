d438a5a44f75a556fc5d39a850c19828733d2d26
Mica <mika89700@gmail.com>
2023-11-26 12:12:39 +0100

    revert unit test

--- tests/test_basic_container.py
+++ tests/test_basic_container.py
@@ -100,7 +100,9 @@ class TestBasicContainer(TestCase):
         )
 
         self.assertEqual(exit_code, 0)
-        self.assertTrue(b'"ConfPath":"conf_docker.yaml"' in output)
+        self.assertTrue(
+            b'"ConfPath":"/etc/qat-computer/conf/conf_docker.yaml"' in output
+        )
 
     def tearDown(self) -> None:
         """TearDown container object."""
