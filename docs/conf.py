731a01a8d22cf06af4b2a05a0bcb40db5d5af228
Michaël Rollin <mika89700@gmail.com>
2024-06-22 10:56:35 +0200

    Others needs (#41)
    Summary
    Add some external interesting packages to have.
    
    Details and comments
     Update apt instal list
     Update python requirements
     Fixe requests version to last one because it's breaking docker python connection
     Bump VERSION

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ author = "Michaël Rollin"
 _rootdir = Path(__file__).parent.parent
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.3.0"
 # The short X.Y version
-version = "0.1"
+version = "0.3"
 
 extensions = [
     "sphinx.ext.napoleon",
